01618b8dfec7d86fd4c535ffbf3d146586061c63
João Vinagre <joao.lima@vilivesoft.com>
2016-05-04 00:18:11 -0300

    update heroku settings

--- grunt/tasks/build.js
+++ grunt/tasks/build.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
     "use strict";
 
-    grunt.registerTask("build", ["sass", "jshint", "newer:jsbeautifier" "copy"]);
+    grunt.registerTask("build", ["sass", "jshint", "newer:jsbeautifier", "copy"]);
 
 };
