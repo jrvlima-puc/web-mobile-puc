f0490c1d89089f6a217ef49ab5ee77ac954e6cc7
João Vinagre <joao.lima@vilivesoft.com>
2016-05-27 00:31:58 -0300

    update mongo

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var config = {
   database: {
-    connectionString: "mongodb://localhost:27017/web-mobile",
+    connectionString: "mongodb://heroku_h4ph4p1q:heroku_h4ph4p1q@ds017173.mlab.com:17173/heroku_h4ph4p1q",
     databaseName: "web-mobile"
   },
   debug: {
