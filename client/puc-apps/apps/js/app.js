7c75c843b42995e443b03666a1f9156dd75a2570
João Vinagre <joao.lima@vilivesoft.com>
2016-05-31 19:24:26 -0300

    add js materialize

--- client/puc-apps/apps/js/app.js
+++ client/puc-apps/apps/js/app.js
@@ -1,26 +1,103 @@
 $(function() {
 
-    $.ajax({
+    var CrudUser = function() {
 
-        url: "/api/users",
+        },
+        crudUser;
 
-        success: function(result) {
+    CrudUser.prototype.renderUserList = function() {
+        var source = $("#list-users-template").html(),
+            template = Handlebars.compile(source),
+            html;
 
-            var source = $("#list-users-template").html(),
-                template = Handlebars.compile(source),
-                users,
-                html;
-
+        var render = function(data) {
             context = {
-                users: result
+                users: data
             };
-
             html = template(context);
-
             $("#content").append(html);
+        };
+
+        var error = function(data) {
+            console.dir(data);
+        };
+
+        this.makeAjaxRequest('GET', render, error);
+    };
+
+    CrudUser.prototype.renderUser = function() {
+        var source = $("#user-template").html(),
+            template = Handlebars.compile(source),
+            html;
+
+        var render = function(data) {
+            context = {
+                users: data
+            };
+            html = template(context);
+            $("#content").append(html);
+        };
+
+        var error = function(data) {
+            console.dir(data);
+        };
+
+        this.makeAjaxRequest('GET', render, error);
+    };
+
+    CrudUser.prototype.renderCreateUser = function() {
+        var source = $("#user-form-template").html(),
+            template = Handlebars.compile(source),
+            html;
+
+        var render = function(data) {
+            context = {
+                users: data
+            };
+            html = template(context);
+            $("#content").append(html);
+        };
+
+        var error = function(data) {
+            console.dir(data);
+        };
+
+        this.makeAjaxRequest('GET', render, error);
+    };
+
+    CrudUser.prototype.craeteUser = function() {
 
 
-        }
-    });
+    };
+
+    CrudUser.prototype.registryEvents = function() {
+
+        $("#add-user").on('click', this.renderCreateUser());
+        $("#delete-user").on('click', this.deleteUser());
+        $("#show-user").on('click', this.showUser());
+
+    }
+
+
+    CrudUser.prototype.makeAjaxRequest = function(method, render, error) {
+        $.ajax({
+            url: "/api/users",
+            method: method,
+            success: function(data) {
+                render(data);
+            },
+            error: function(data) {
+                error(data);
+            }
+        });
+    }
+
+    CrudUser.prototype.init = function() {
+        this.renderUserList();
+    }
+
+    crudUser = new CrudUser();
+
+    crudUser.init();
 
 });
--- grunt/sass.js
+++ grunt/sass.js
@@ -1,21 +1,12 @@
 module.exports = {
     development: {
         options: {
-            style: "expanded",
-            noCache: true
-        },
-        files: {
-            // "client/template/css/main.css": "client/template/sass/main.scss",
-            "client/puc-apps/app/css/main.css": "client/puc-apps/app/sass/main.scss"
-        }
-    },
-    template: {
-        options: {
             style: "expanded"
         },
         files: {
             // "client/template/css/main.css": "client/template/sass/main.scss",
-            "client/template/css/main.css": "client/template/sass/main.scss"
+            "client/puc-apps/apps/css/main.css": "client/puc-apps/apps/sass/main.scss",
+            "client/puc-apps/apps/css/simple.css": "client/puc-apps/apps/sass/simple.scss"
         }
     }
 };
