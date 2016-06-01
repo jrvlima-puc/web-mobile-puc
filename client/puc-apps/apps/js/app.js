373adde8b902bc8ed362a7783ab33dd62a828cbc
João Vinagre <joao.lima@vilivesoft.com>
2016-06-01 14:02:09 -0300

    update app.js and add fonts

--- client/puc-apps/apps/js/app.js
+++ client/puc-apps/apps/js/app.js
@@ -5,83 +5,92 @@ $(function() {
         },
         crudUser;
 
+    /**
+     * @method renderUserList
+     *
+     * Essse método é responsável por:
+     *
+     * - selectionar a tag script com o template handlebars, que se encontra em index.html/index-material.html,
+     * - compilar o template
+     * - declarar o metódo para renderizar o template na template
+     * - declarar o metódo para logar possível erro na requisição ajax
+     * - chamar o método responsável por requisições ajax
+     *
+     */
     CrudUser.prototype.renderUserList = function() {
         var source = $("#list-users-template").html(),
             template = Handlebars.compile(source),
             html;
 
+        /**
+         * @callback render
+         */
         var render = function(data) {
             context = {
                 users: data
             };
             html = template(context);
             $("#content").append(html);
+            registryEvents();
         };
 
+        /**
+         * @callback error
+         */
         var error = function(data) {
             console.dir(data);
+        };
+
+        var registryEvents = function() {
+            $("#add-user").on('click', this.renderCreateUser());
+            $("#delete-user").on('click', this.deleteUser());
+            $("#show-user").on('click', this.showUser());
         };
 
         this.makeAjaxRequest('GET', render, error);
     };
 
+
+    /**
+     * Implementar método para mostrar um usuário
+     */
     CrudUser.prototype.renderUser = function() {
-        var source = $("#user-template").html(),
-            template = Handlebars.compile(source),
-            html;
 
-        var render = function(data) {
-            context = {
-                users: data
-            };
-            html = template(context);
-            $("#content").append(html);
-        };
-
-        var error = function(data) {
-            console.dir(data);
-        };
-
-        this.makeAjaxRequest('GET', render, error);
     };
 
+    /**
+     * Implementar método para renderizar form
+     */
     CrudUser.prototype.renderCreateUser = function() {
-        var source = $("#user-form-template").html(),
-            template = Handlebars.compile(source),
-            html;
-
-        var render = function(data) {
-            context = {
-                users: data
-            };
-            html = template(context);
-            $("#content").append(html);
-        };
-
-        var error = function(data) {
-            console.dir(data);
-        };
-
-        this.makeAjaxRequest('GET', render, error);
-    };
-
-    CrudUser.prototype.craeteUser = function() {
-
 
     };
 
-    CrudUser.prototype.registryEvents = function() {
+    /**
+     * Implementar método para salvar um usuário, action do button submit
+     */
+    CrudUser.prototype.saveUser = function() {
 
-        $("#add-user").on('click', this.renderCreateUser());
-        $("#delete-user").on('click', this.deleteUser());
-        $("#show-user").on('click', this.showUser());
+    };
 
-    }
+    /**
+     * Implementar método para remover um usuário, action do link delete
+     */
+    CrudUser.prototype.deleteUser = function() {
 
+    };
 
-    CrudUser.prototype.makeAjaxRequest = function(method, render, error) {
+    /**
+     * Esse método é utilizado para executar as requisição ajax
+     *
+     * @param {string} method - envia uma string com o método HTTP - eg: 'GET', 'POST'
+     * @param {render} render
+     * @param {error} error
+     * @param {string} params - Request parameters - eg: /users/9823498273
+     *
+     */
+    CrudUser.prototype.makeAjaxRequest = function(method, render, error, params) {
         $.ajax({
-            url: "/api/users",
+            url: "/api/users" + (params? ("/" + params): ""),
             method: method,
             success: function(data) {
                 render(data);
@@ -92,6 +101,10 @@ $(function() {
         });
     }
 
+    /**
+     * Esse método é chamado para inicializar a página
+     * HOME que no nosso caso é a lista de usuários
+     */
     CrudUser.prototype.init = function() {
         this.renderUserList();
     }
--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ require('./config/mongo');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var userRouter = require('./routes/userRouter.js');
 
 app.use(logger('dev'));
